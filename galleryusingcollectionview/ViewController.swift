f0885bdb8171b3e21a81446030ac5e91c29af047
vigneshkumarzx <vigneshzx@gmail.com>
2021-11-18 16:34:38 +0530

    unwanted function removed

--- galleryusingcollectionview/ViewController.swift
+++ galleryusingcollectionview/ViewController.swift
@@ -103,16 +103,6 @@ extension ViewController: UICollectionViewDelegateFlowLayout {
             CGSize(width: (view.frame.width / 2), height: 80)
         }
 
-func collectionView(_ collectionView: UICollectionView,
-                        layout collectionViewLayout: UICollectionViewLayout,
-                       minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 0
-   }
-
-   func collectionView(_ collectionView: UICollectionView,
-                      layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-       return 0
-  }
   
 }
 
